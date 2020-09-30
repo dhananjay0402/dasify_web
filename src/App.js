3f2be0e5613eaec49397b818c5e671b043414b41
dhananjay0402 <dhananjaysharma0402@gmail.com>
2020-09-30 13:27:21 +0530

    kuch bhi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Ournews from "./Ournews";
+import "./App.css";
+import OurTeam from "./OurTeam";
+
+function App() {
+  return (
+    <>
+      <OurTeam />
+    </>
+  );
+}
+
+export default App;
